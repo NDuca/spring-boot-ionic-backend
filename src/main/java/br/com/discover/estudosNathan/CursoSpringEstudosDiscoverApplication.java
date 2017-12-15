c6de5abafcdf238cb7d3402c6a5a6210f2799202
Nathan Duca <nathanduca10@gmail.com>
2017-12-15 16:41:09 -0200

    Projeto Criado

---
+++ src/main/java/br/com/discover/estudosNathan/CursoSpringEstudosDiscoverApplication.java
@@ -0,0 +1,12 @@
+package br.com.discover.estudosNathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringEstudosDiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringEstudosDiscoverApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/discover/estudosNathan/CursoSpringEstudosDiscoverApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.discover.estudosNathan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoSpringEstudosDiscoverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
