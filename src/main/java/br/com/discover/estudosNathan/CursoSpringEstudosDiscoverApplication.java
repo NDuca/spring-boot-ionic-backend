da8f5faa62e5a0b4d1f43ceb9ce9e40ab45fe7fd
Nathan Duca <nathanduca10@gmail.com>
2017-12-18 16:08:47 -0200

    Criando operacao de instanciacao

--- src/main/java/br/com/discover/estudosNathan/CursoSpringEstudosDiscoverApplication.java
+++ src/main/java/br/com/discover/estudosNathan/CursoSpringEstudosDiscoverApplication.java
@@ -1,12 +1,30 @@
 package br.com.discover.estudosNathan;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.discover.estudosNathan.domain.Categoria;
+import br.com.discover.estudosNathan.repositories.CategoriaRepository;
 
 @SpringBootApplication
-public class CursoSpringEstudosDiscoverApplication {
-
+public class CursoSpringEstudosDiscoverApplication implements CommandLineRunner{
+// CommandLineRunner permite implementar um metodo auxiliar
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringEstudosDiscoverApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
 	}
 }
