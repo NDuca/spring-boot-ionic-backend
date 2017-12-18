036fef7deddef381e07f6ca9778de69276f1151d
Nathan Duca <nathanduca10@gmail.com>
2017-12-18 17:22:28 -0200

    Ajustes no endpoint /categorias/id

--- src/main/java/br/com/discover/estudosNathan/services/CategoriaService.java
+++ src/main/java/br/com/discover/estudosNathan/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.discover.estudosNathan.domain.Categoria;
 import br.com.discover.estudosNathan.repositories.CategoriaRepository;
+import br.com.discover.estudosNathan.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,11 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado. id: " + id
+					+ " , Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
