7eb829c0e0aded86776dcbb7cb1a6bc440d41228
Nathan Duca <nathanduca10@gmail.com>
2017-12-19 16:34:38 -0200

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/discover/estudosNathan/CursoSpringEstudosDiscoverApplication.java
+++ src/main/java/br/com/discover/estudosNathan/CursoSpringEstudosDiscoverApplication.java
@@ -11,6 +11,7 @@ import br.com.discover.estudosNathan.domain.Cidade;
 import br.com.discover.estudosNathan.domain.Cliente;
 import br.com.discover.estudosNathan.domain.Endereco;
 import br.com.discover.estudosNathan.domain.Estado;
+import br.com.discover.estudosNathan.domain.ItemPedido;
 import br.com.discover.estudosNathan.domain.Pagamento;
 import br.com.discover.estudosNathan.domain.PagamentoComBoleto;
 import br.com.discover.estudosNathan.domain.PagamentoComCartao;
@@ -23,6 +24,7 @@ import br.com.discover.estudosNathan.repositories.CidadeRepository;
 import br.com.discover.estudosNathan.repositories.ClienteRepository;
 import br.com.discover.estudosNathan.repositories.EnderecoRepository;
 import br.com.discover.estudosNathan.repositories.EstadoRepository;
+import br.com.discover.estudosNathan.repositories.ItemPedidoRepository;
 import br.com.discover.estudosNathan.repositories.PagamentoRepository;
 import br.com.discover.estudosNathan.repositories.PedidoRepository;
 import br.com.discover.estudosNathan.repositories.ProdutoRepository;
@@ -54,6 +56,9 @@ public class CursoSpringEstudosDiscoverApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringEstudosDiscoverApplication.class, args);
@@ -119,5 +124,19 @@ public class CursoSpringEstudosDiscoverApplication implements CommandLineRunner{
 		
 		pedidoRepository.save(Arrays.asList(ped1, ped2));
 		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.save(Arrays.asList(ip1, ip2, ip3));
+		
 	}
 }
