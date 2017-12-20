3a0a3703dbb53dc2b363bbe4d338e1779c84bb23
Nathan Duca <nathanduca10@gmail.com>
2017-12-20 11:39:52 -0200

    Deletando uma Categoria com DELETE e tratamento de erro

--- src/main/java/br/com/discover/estudosNathan/services/CategoriaService.java
+++ src/main/java/br/com/discover/estudosNathan/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package br.com.discover.estudosNathan.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.discover.estudosNathan.domain.Categoria;
 import br.com.discover.estudosNathan.repositories.CategoriaRepository;
+import br.com.discover.estudosNathan.services.exceptions.DataIntegrityException;
 import br.com.discover.estudosNathan.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,6 +33,19 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+		
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			
+			repo.delete(id);
+			
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			
+		}
 		
 	}
 
